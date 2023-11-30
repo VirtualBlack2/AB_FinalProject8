9f9bae85dcf9ff5a3c7c4440e5dd053c2058359d
VirtualBlack2 <BBbrownstudent1400@gmail.com>
2023-11-30 13:25:04 -0500

    1

--- froggerhelper.py
+++ froggerhelper.py
@@ -1,5 +1,3 @@
-
-
 import arcade
 import random
 
@@ -10,31 +8,63 @@ VEHICLE_SIZE = 50
 TURTLE_SIZE = 50
 SAFE_ZONE_HEIGHT = 100
 
+
 # Function to initialize vehicles
 def create_vehicles(direction, count):
     vehicles = arcade.SpriteList()
     for _ in range(count):
-        vehicle = arcade.Sprite("vehicle.png", 0.1)  # Updated to use vehicles.png
-        vehicle.center_x = random.randint(0, WINDOW_WIDTH)
+        vehicle = arcade.Sprite("vehicle.png", 0.1)
+        if direction == -1:
+            vehicle.center_x = random.randint(WINDOW_WIDTH // 2, WINDOW_WIDTH)
+        else:
+            vehicle.center_x = random.randint(0, WINDOW_WIDTH // 2)
         vehicle.center_y = random.randint(SAFE_ZONE_HEIGHT, WINDOW_HEIGHT - SAFE_ZONE_HEIGHT)
-        vehicle.change_x = direction * 5  # Update to set the direction
         vehicles.append(vehicle)
     return vehicles
+
 
 # Function to initialize turtles
 def create_turtles(direction, count):
     turtles = arcade.SpriteList()
     for _ in range(count):
-        turtle = arcade.Sprite("turtle.png", 0.1)  # Updated to use turtle.png
-        turtle.center_x = random.randint(0, WINDOW_WIDTH)
+        turtle = arcade.Sprite("turtle.png", 0.1)
+        if direction == -1:
+            turtle.center_x = random.randint(WINDOW_WIDTH // 2, WINDOW_WIDTH)
+        else:
+            turtle.center_x = random.randint(0, WINDOW_WIDTH // 2)
         turtle.center_y = random.randint(SAFE_ZONE_HEIGHT, WINDOW_HEIGHT - SAFE_ZONE_HEIGHT)
-        turtle.change_x = direction * 3  # Update to set the direction
         turtles.append(turtle)
     return turtles
 
 # Function to initialize player
 def create_player():
-    player = arcade.Sprite("frog.png", 0.1)  # Updated to use frog.png
+    player = arcade.Sprite("frog.png", 0.1)
     player.center_x = WINDOW_WIDTH // 2
     player.center_y = SAFE_ZONE_HEIGHT // 2
     return player
+
+
+# Function to update vehicles' positions
+def update_vehicles(vehicles, direction):
+    for vehicle in vehicles:
+        if direction == -1:
+            vehicle.center_x -= 1
+            if vehicle.right < 0:
+                vehicle.left = WINDOW_WIDTH // 2
+        else:
+            vehicle.center_x += 1
+            if vehicle.left > WINDOW_WIDTH // 2:
+                vehicle.right = WINDOW_WIDTH
+
+
+# Function to update turtles' positions
+def update_turtles(turtles, direction):
+    for turtle in turtles:
+        if direction == -1:
+            turtle.center_x -= 1
+            if turtle.right < 0:
+                turtle.left = WINDOW_WIDTH // 2
+        else:
+            turtle.center_x += 1
+            if turtle.left > WINDOW_WIDTH // 2:
+                turtle.right = WINDOW_WIDTH
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
-
 import arcade
-from froggerhelper import create_player, create_vehicles, create_turtles
+from froggerhelper import create_player, create_vehicles, create_turtles, WINDOW_WIDTH, WINDOW_HEIGHT, SAFE_ZONE_HEIGHT
+from froggerhelper import update_vehicles, update_turtles
 
 class FroggerGame(arcade.Window):
     def __init__(self):
@@ -12,9 +11,13 @@ class FroggerGame(arcade.Window):
         self.turtles_left_sprites = create_turtles(-1, 3)
         self.turtles_right_sprites = create_turtles(1, 3)
         self.jump_sound = arcade.load_sound("jump_sound.wav")
+        self.win_sound = arcade.load_sound("win_sound.wav")
+        self.player_won = False
+        self.win_text = arcade.Text("You Win!", self.width // 2, self.height // 2,
+                                    arcade.color.BLACK, font_size=36, width=400, align="center", anchor_x="center")
 
     def setup(self):
-        arcade.set_background_color(arcade.color.BLUE)
+        arcade.set_background_color(arcade.color.GO_GREEN)
 
     def on_draw(self):
         arcade.start_render()
@@ -24,14 +27,50 @@ class FroggerGame(arcade.Window):
         self.vehicles_right.draw()
         self.player.draw()
 
-    def update(self, delta_time):
-        self.turtles_left_sprites.update()
-        self.turtles_right_sprites.update()
-        self.vehicles_left.update()
-        self.vehicles_right.update()
-        self.player.update()
+        if self.player_won:
+            self.win_text.draw()
 
-        # Check for collisions here
+    def update(self, delta_time):
+        if not self.player_won:
+            update_vehicles(self.vehicles_left, -1)
+            update_vehicles(self.vehicles_right, 1)
+            update_turtles(self.turtles_left_sprites, -1)
+            update_turtles(self.turtles_right_sprites, 1)
+
+            self.player.update()
+
+            for vehicle in self.vehicles_left:
+                if vehicle.right < 0:
+                    vehicle.left = self.width
+            for vehicle in self.vehicles_right:
+                if vehicle.left > self.width:
+                    vehicle.right = 0
+
+            # Check and reset turtles that move out of the window
+            for turtle in self.turtles_left_sprites:
+                if turtle.right < 0:
+                    turtle.left = self.width
+            for turtle in self.turtles_right_sprites:
+                if turtle.left > self.width:
+                    turtle.right = 0
+
+            if arcade.check_for_collision_with_list(self.player, self.vehicles_left):
+                self.reset_player_position()
+
+            if arcade.check_for_collision_with_list(self.player, self.vehicles_right):
+                self.reset_player_position()
+
+            # Check for collisions with turtles
+            if arcade.check_for_collision_with_list(self.player, self.turtles_left_sprites):
+                self.reset_player_position()
+
+            if arcade.check_for_collision_with_list(self.player, self.turtles_right_sprites):
+                self.reset_player_position()
+
+            # Check if the player has reached the top of the screen (win condition)
+            if self.player.center_y > WINDOW_HEIGHT - SAFE_ZONE_HEIGHT:
+                self.player_won = True
+                arcade.play_sound(self.win_sound)
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.W:
@@ -49,6 +88,11 @@ class FroggerGame(arcade.Window):
             self.player.change_y = 0
         elif key == arcade.key.A or key == arcade.key.D:
             self.player.change_x = 0
+
+    def reset_player_position(self):
+        self.player.center_x = WINDOW_WIDTH // 2
+        self.player.center_y = SAFE_ZONE_HEIGHT // 2  # Reset player position
+        arcade.play_sound(arcade.load_sound("lose_sound.wav"))
 
 def main():
     window = FroggerGame()
